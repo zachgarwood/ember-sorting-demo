fe81c6272e32d347ef3245a24d53ff2914b89243
Zach Garwood <zachgarwood@gmail.com>
2016-02-19 15:45:32 -0600

    Added reverse sort ordering

--- app/components/review-list.js
+++ app/components/review-list.js
@@ -4,7 +4,9 @@ export default Ember.Component.extend({
   tagName: 'ul',
   sortedReviews: Ember.computed.sort('reviews', 'sortDefinition'),
   sortBy: 'date', // default sort by date
-  sortDefinition: Ember.computed('sortBy', function() {
-    return [ this.get('sortBy') ];
+  reverseSort: false, //default sort in ascending order
+  sortDefinition: Ember.computed('sortBy', 'reverseSort', function() {
+    let sortOrder = this.get('reverseSort') ? 'desc' : 'asc';
+    return [ `${this.get('sortBy')}:${sortOrder}` ];
   }),
 });
